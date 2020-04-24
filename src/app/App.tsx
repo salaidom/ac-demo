542466fc44007ffc74bf29dc169833d2705a2894
Dominik Salai <dominik.salai@gmail.com>
2020-04-24 13:01:09 +0200

    Added MaterialUI and initialized application theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import theme from '../config/theme'
 
 const App: React.FC = () => {
-  return <h1>Application</h1>
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography variant="h1">Application</Typography>
+    </ThemeProvider>
+  )
 }
 
 export default App
