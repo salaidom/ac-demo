2539b64e5f5ed5535abccb55d527ab29412be9bc
Dominik Salai <dominik.salai@gmail.com>
2020-04-27 09:36:02 +0200

    Added apollo cache

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { NormalizedCacheObject } from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
 
 import theme from 'src/config/theme'
 import Layout from 'src/components/Layout'
 import { SelectedFileContext, TaxonomyContext } from './Context'
 import { Taxonomy } from '../types/taxonomy'
+import { PersistentStorage, PersistedData } from 'apollo-cache-persist/types'
 
-const client = new ApolloClient({
-  uri: 'https://react-test.atlasconsulting.cz/',
-})
-
-const App: React.FC = () => {
+// eslint-disable-next-line
+const App: React.FC = props => {
+  // eslint-disable-next-line
+  const [apolloClient, setApolloClient] = useState<
+    ApolloClient<unknown> | undefined
+  >(undefined)
   const [fileId, setFileId] = useState<string>('')
   const [taxonomy, setTaxonomy] = useState<Taxonomy[]>([])
 
+  useEffect(() => {
+    const cache = new InMemoryCache()
+    const client = new ApolloClient({
+      cache,
+      uri: 'https://react-test.atlasconsulting.cz/',
+    })
+
+    persistCache({
+      cache,
+      storage: window.localStorage as PersistentStorage<
+        PersistedData<NormalizedCacheObject>
+      >,
+    }).then(() => {
+      setApolloClient(client)
+    })
+    return () => {}
+  }, [])
+
+  if (apolloClient === undefined) return <div>Loading data from cache...</div>
+
   return (
     <ThemeProvider theme={theme}>
-      <ApolloProvider client={client}>
+      <ApolloProvider client={apolloClient}>
         <SelectedFileContext.Provider value={{ fileId, setFileId }}>
           <TaxonomyContext.Provider value={{ taxonomy, setTaxonomy }}>
             <Layout />
